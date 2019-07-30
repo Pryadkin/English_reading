3573f4b76bd730b03707658be8616d00c78f50ae
pryadkin.a <pryadkin.a@citilink.ru>
2019-07-30 18:42:05 +0300

    add regexp

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-textArea = document.querySelector('textarea');
-but = document.querySelector('button a');
-output = document.querySelector('.output');
-butChangeFonts = document.querySelector('.butChangeFonts'); 
-butStartReading = document.querySelector('.butStartReading'); 
-hidden = document.querySelector('.hidden');
-overlay = document.querySelector('.hidden');
+const
+   textArea = document.querySelector('textarea'),
+   but = document.querySelector('button a'),
+   output = document.querySelector('.output'),
+   butChangeFonts = document.querySelector('.butChangeFonts'), 
+   butStartReading = document.querySelector('.butStartReading'), 
+   hidden = document.querySelector('.hidden'),
+   overlay = document.querySelector('.hidden'),
+   butExit = document.querySelector('.butExit');
+
 
 
 but.onclick = () => {
@@ -12,14 +15,15 @@ but.onclick = () => {
    textArea.value = textArea.value.replace(/�/g, '');
    output.innerHTML = textArea.value;  
 
-   overlay.classList.remove('hidden');   
+   overlay.classList.remove('hidden');
    butChangeFonts.classList.remove('hidden');   
    butStartReading.classList.remove('hidden');   
 
-   console.log( output.getBoundingClientRect().height)
-   console.log(window)
+   // console.log( output.getBoundingClientRect().height)
+   // console.log(window)
    // element.scrollTop = '1000px';
    // console.log(element.scrollTop)
+   getAmountWords();
 };
 
 let i = 1;
@@ -32,18 +36,68 @@ butChangeFonts.onclick = () => {
    if (i < fonts.length) {
       i++;         
    } else i = 0;
-   output.style.fontFamily = fonts[i]; 
+   output.style.fontFamily = fonts[i];
 }
 
 
 let step = 0;
-butStartReading.onclick = function move() { 
+butStartReading.onclick = function move() {
    step += 0.3;
-   
+
    window.scrollTo({
       top: step,
       behavior: 'auto'
    });
 
-   if (step < 5000) requestAnimationFrame(move);   
+   if (step < 5000) requestAnimationFrame(move);
+}
+
+butExit.onclick = function() {
+   overlay.classList.add('hidden');
+}
+
+function getAmountWords() {
+   const amountWord = document.querySelector('.amountWord');
+   let set = new Set();
+   let str = textArea.value.trim();
+   let regexp = /\w+/ig;
+
+
+
+   let result;
+   // let i = 0;
+   while (regexp.exec(str)) {
+   // while (i < 200) {
+      
+      // result = regexp.exec(str);
+      // if (result !== null) {
+         set.add(result[0].toLowerCase());
+      // }
+      result = regexp.exec(str);
+      // i++;
+   }
+   // }
+
+   console.log(set)
+
+
+
+
+   // console.log(result[0]);
+   // console.log(result.index);
+   // console.log(regexp.lastIndex);
+
+   // result = regexp.exec(str);
+  
+   // console.log(result);
+   // console.log(result.index);
+   // console.log(regexp.lastIndex);
+   // let result = textArea.value.match(/\w+/);
+
+   // console.log(result.index)
+   // console.log(textArea.value.match(/\w+/))
+
+   
+
+   
 }
