b374f840cfec612dfaa3862e76b798d5843b090e
pryadkin.a <pryadkin.a@citilink.ru>
2019-08-15 16:32:56 +0300

    add table for words

--- script.js
+++ script.js
@@ -8,33 +8,38 @@ const
    overlay = document.querySelector('.hidden'),
    butExit = document.querySelector('.butExit');
 
+let
+   arrayAllWords = [];
+
 
 
 but.onclick = () => {
    textArea.value = textArea.value.replace(/\n/g, '<br>');
    textArea.value = textArea.value.replace(/�/g, '');
-   output.innerHTML = textArea.value;  
+   output.innerHTML = textArea.value;
 
    overlay.classList.remove('hidden');
-   butChangeFonts.classList.remove('hidden');   
-   butStartReading.classList.remove('hidden');   
+   butChangeFonts.classList.remove('hidden');
+   butStartReading.classList.remove('hidden');
 
    // console.log( output.getBoundingClientRect().height)
    // console.log(window)
    // element.scrollTop = '1000px';
    // console.log(element.scrollTop)
    getAmountWords();
+   getColorMarkWord();
 };
 
 let i = 1;
-butChangeFonts.onclick = () => {   
+butChangeFonts.onclick = () => {
    let fonts = [
       'Mali',
       'Cherry Swash',
-      'Alike Angular',      
+      'Alike Angular',
+      'HPSimplified_Rg',
    ];
-   if (i < fonts.length) {
-      i++;         
+   if (i < fonts.length - 1) {
+      i++;
    } else i = 0;
    output.style.fontFamily = fonts[i];
 }
@@ -56,57 +61,94 @@ butExit.onclick = function() {
    overlay.classList.add('hidden');
 }
 
+// function display defferent words
+
 function getAmountWords() {
    const amountWord = document.querySelector('.amountWord');
-   let arrayAllWords = [];
+   let countAllWords = 0; //кол-во слов в тексте
    let wordObj = {};
    let set = new Set();
    let str = textArea.value.trim();
-   let regexp = /\w+/ig;
-
-
+   let regexp = /[a-z]+\b/ig;  // Выражение - /\w+/ig; - ищет все слова, в том числе и из цифр. Чтобы цифры отсеить нажно искать так - /[a-z]+/ig;
 
    // Присваиваем regexp.exec(str) переменной, т.к. любой лишний вызов (даже в console.log) повлияет на полученный результат
    let result;
    while (result = regexp.exec(str)) {
-      set.add(result[0].toLowerCase());
+      set.add(result[0].toLowerCase()); // result[0] - первый элемент этого массива - слово (оно нам и нужно)
+      countAllWords += result[0]; // счет количества слов в тексте
    }
 
    for (let wordSet of set) {
-      var reg = new RegExp("" + wordSet +"", "gi");
-      let arrayWord = str.match(reg);
+      var reg = new RegExp("\\b" + wordSet +"\\b", "gi");
+      let arrayWord = str.match(reg); // при каждой итерации достаем уникальное слово из Set, находим его количество в тексте и записываем в массив arrayWord
 
       function Word(name, amount) {
          this.name = name;
          this.amount = amount;
       }
 
-      let wordObj = new Word(arrayWord[0], arrayWord.length)
-
+      wordObj = new Word(arrayWord[0], arrayWord.length);
       arrayAllWords.push(wordObj);
    }
 
-   function sortWords(prop) {
-      arrayAllWords.sort((a, b) => {
-         if (isNaN(a[prop])) {
-            if (a[prop].toLowerCase() > b[prop].toLowerCase()) {
-               return 1;
-            } else return -1;
-         } else {
-            if (a[prop] < b[prop]) {
-               return 1;
-            } else return -1;
-         }
-      })
-   }
    sortWords('amount');
 
+   createTable(50);
+
+   showCountWordInText();
+   function showCountWordInText() {
+      let amountAllWords = document.querySelector('.amountAllWords');
+      amountAllWords.innerHTML = `Amount all words: ${countAllWords.length}.`;
+   }
+}
 
 
-   console.log(arrayAllWords)
 
-
-   arrayAllWords.forEach((i) => {
-      amountWord.innerHTML += `<li>${i.name.toLowerCase()}(${i.amount})</li>`
+// Сортировка
+function sortWords(prop) {
+   arrayAllWords.sort((a, b) => {
+      if (isNaN(a[prop])) {
+         if (a[prop].toLowerCase() > b[prop].toLowerCase()) {
+            return 1;
+         } else return -1;
+      } else {
+         if (a[prop] < b[prop]) {
+            return 1;
+         } else return -1;
+      }
    })
 }
+
+
+function createTable(count) {
+   const table = document.querySelector('.table');
+
+   arrayAllWords.forEach((i, index) => {
+      let tab;
+      if (index % count == 0) {
+         tab = document.createElement('div');
+         tab.innerHTML = `<table></table>`;
+         table.appendChild(tab);
+      }
+      table.lastChild.lastChild.innerHTML += `
+      <tr>
+         <th>${i.name.toLowerCase()}</th>
+         <th>${i.amount}</th>
+      </tr>
+      `
+   })
+}
+
+
+function getColorMarkWord() {
+   const word = 'the';
+
+   let regexp = new RegExp("\\b" + word + "\\b", "g");
+   output.innerHTML = output.innerHTML.replace(regexp, `<span class="color">${word}</span>`);
+
+   const wordUpper = word.slice(0, 1).toUpperCase() + word.slice(1);
+   regexp = new RegExp("\. " + wordUpper + "\\b", "g");
+   output.innerHTML = output.innerHTML.replace(regexp, `. <span class="color">${wordUpper}</span>`);
+}
+
+
