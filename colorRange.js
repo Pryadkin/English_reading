3cdd14cf1b073ae915db1ab35212c1886a11645d
pryadkin.a <pryadkin.a@citilink.ru>
2019-08-23 12:46:59 +0300

    change variables a, b, c onto r, g, b

--- colorRange.js
+++ colorRange.js
@@ -1,14 +1,14 @@
 
-function getColorBackground(a, b, c) {
+function getColorBackground(r, g, b) {
     const   colorRange = document.querySelector('.colorRange'),
             body = document.body;           
             
 
     console.log(colorRange.elements[0].type)
-    colorRange.elements[0].value = a;
-    colorRange.elements[1].value = b;
-    colorRange.elements[2].value = c;
-    body.style.background = `rgb(${a}, ${b}, ${c}, .5)`;
+    colorRange.elements[0].value = r;
+    colorRange.elements[1].value = g;
+    colorRange.elements[2].value = b;
+    body.style.background = `rgb(${r}, ${g}, ${b})`;
     // if (colorRange.children[0])
     // let att = colorRange.children[0].children.getAttribute('min');
     // console.log(att);
@@ -16,21 +16,21 @@ function getColorBackground(a, b, c) {
     // console.log(colorRange.children[0].children);
 
     colorRange.addEventListener("input", (e) => {
-        if (e.target.dataset.num == 'a') {
-            a = e.target.value;
+        if (e.target.dataset.num == 'r') {
+            r = e.target.value;
+        }
+        if (e.target.dataset.num == 'g') {
+            g = e.target.value;
         }
         if (e.target.dataset.num == 'b') {
             b = e.target.value;
         }
-        if (e.target.dataset.num == 'c') {
-            c = e.target.value;
-        }
         
-        body.style.background = `rgb(${a}, ${b}, ${c})`;
+        body.style.background = `rgb(${r}, ${g}, ${b})`;
     })
 }
 
-getColorBackground(200, 180, 150);
+getColorBackground(100, 100, 100);
 
 
 
